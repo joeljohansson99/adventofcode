2b0bdc25155b4a946997205d078af44ea8eba98e
joel.johansson <joel.k.johansson@gmail.com>
2021-12-01 08:27:31 +0100

    Day1 2021

---
+++ 2021/src/Day1.hs
@@ -0,0 +1,33 @@
+import Data.List.Split
+
+part1 = do 
+    contents <- readFile "input/day1.txt"
+    let depts = mapToInt contents
+    print . countInc $ depts
+
+part2 = do
+    contents <- readFile "input/day1.txt"
+    let depts = mapToInt contents
+    print . countSums $ depts
+
+mapToInt :: String -> [Int]
+mapToInt = map rInt . words
+
+rInt :: String -> Int
+rInt = read
+
+countInc :: [Int] -> Int
+countInc [x] = 0;
+countInc (x:xs) 
+    | x <= head xs = 1 + countInc xs
+    | otherwise = countInc xs
+
+countSums :: [Int] -> Int
+countSums [x,_,_] = 0
+countSums (x:xs)
+    | (sumOf 3 (x:xs)) < (sumOf 3 xs) = 1 + countSums xs
+    | otherwise = countSums xs
+
+sumOf :: Int -> [Int] -> Int
+sumOf 0 _ = 0
+sumOf n (x:xs) = x + (sumOf (n-1) xs)

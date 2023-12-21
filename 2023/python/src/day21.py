91f3b196d5550ce959b6f66cdae4e3509d23a1ba
Joel Johansson <joel.k.johansson@gmail.com>
2023-12-21 15:56:09 +0100

    day21

--- 2023/python/src/day21.py
+++ 2023/python/src/day21.py
@@ -25,24 +25,20 @@ def part1(input):
     
 def part2(input):
     start = (0,0)
-    spaces = 0
     for r in range(0, len(input)):
         for c in range(0, len(input[0])):
-            if input[r][c] != '#':
-                spaces+=1
             if input[r][c] == 'S':
                 start = (r,c)
     
     w = len(input)
     middle = (w//2)
     
-    steps = middle+w
+    steps = 26501365
     reach = (steps-middle) // w
-    left = (steps-middle-1) % w
-    print(left)
-    
+
     even = bfs(input, start, 2*w, 0)
-    odd = spaces - even
+    
+    odd = bfs(input, start, 2*w, 1)
     
     bottom_left_64 = bfs(input, (w-1,0), 64, 0)
     bottom_right_64 = bfs(input, (w-1,w-1), 64, 0)
@@ -54,11 +50,11 @@ def part2(input):
     upper_left_195 = bfs(input, (0,0), 195, 1)
     upper_right_195 = bfs(input, (0,w-1), 195, 1)
     
-    middle_left = bfs(input, (middle, w-1), 130, 1)
-    middle_right = bfs(input, (middle, 0), 130, 1)
-    middle_down = bfs(input, (0, middle), 130, 1)
-    middle_up = bfs(input, (middle, w-1), 130, 1)
-    
+    middle_left = bfs(input, (middle, w-1), 130, 0)
+    middle_right = bfs(input, (middle, 0), 130, 0)
+    middle_down = bfs(input, (0, middle), 130, 0)
+    middle_up = bfs(input, (w-1, middle), 130, 0)
+
     ret = odd
     
     for i in range(1,reach):
@@ -66,21 +62,19 @@ def part2(input):
             ret += 4*i*odd
         else: 
             ret += 4*i*even
-    print(ret, odd, even)
+    
     ret += middle_down + middle_left + middle_up + middle_right
-    print(ret, odd, even)
     
     left195 = reach*4 - 4
     left64 = left195+4
     
-    print(reach, left64, left195)
     for i in range(0, int(left195//4)):
         ret += bottom_left_195 + bottom_right_195 + upper_left_195 + upper_right_195
     
     for i in range(0, int(left64//4)):
         ret += bottom_left_64 + bottom_right_64 + upper_left_64 + upper_right_64
     
-    return ret, bfs2(input, start, steps, steps % 2)
+    return ret
     
 def bfs(map, start, steps, m):
     current = set()
@@ -97,29 +91,6 @@ def bfs(map, start, steps, m):
             neigh = [(r+1, c),(r-1, c), (r, c+1),(r, c-1)]
             for n in neigh:
                 if n not in visited and n[0] >= 0 and n[0] < len(map) and n[1] >= 0 and n[1] < len(map[0]) and map[n[0]][n[1]] != "#":
-                    if (i+1) % 2 == m:
-                        count += 1
-                    next.add(n)
-                    visited.add(n)
-        i += 1
-        current = next
-    return count
-
-def bfs2(map, start, steps, m):
-    current = set()
-    visited = set()
-    
-    current.add(start)
-    visited.add(start)
-    
-    count = (1 + m) % 2
-    i = 0
-    while i < steps:
-        next = set()
-        for (r,c) in current:
-            neigh = [(r+1, c),(r-1, c), (r, c+1),(r, c-1)]
-            for n in neigh:
-                if n not in visited and map[n[0]%len(map)][n[1]%len(map)] != "#":
                     if (i+1) % 2 == m:
                         count += 1
                     next.add(n)
